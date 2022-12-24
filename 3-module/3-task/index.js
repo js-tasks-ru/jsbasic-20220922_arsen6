331687b9c79443e1295ae05ae286354aff4cbe21
Arsen Omarov <96700475+arsenomarov@users.noreply.github.com>
2022-12-24 21:21:25 +0400

    module3-3/3-4

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+    let arr = str.split( "" );
+  arr.forEach( function( item, index, array ) {
+    if (item === '-') {
+      array[ index + 1 ] = array[ index + 1 ].toUpperCase();
+      array.splice( index, 1 );
+    }
+  });
+  arr = arr.join( '' );
+  return arr;
 }
