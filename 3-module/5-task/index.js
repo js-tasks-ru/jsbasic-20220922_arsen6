7d0d03a1773fc472752033e74f02f5de414584d1
Arsen Omarov <96700475+arsenomarov@users.noreply.github.com>
2023-01-02 20:25:49 +0400

    module3-5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str
+  .split( " " )
+  .map( item => +item )
+  .filter( function( a ){ return !( a !== a ); } );
+  let minmax = {min: Math.min( ...arr ),
+  max: Math.max( ...arr )}
+
+return minmax
 }
